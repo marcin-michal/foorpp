00afa6d7700a6e75451034f0b56b567fb9da7368
xmarcin3 <xmarcin3@fi.muni.cz>
2023-04-29 17:51:40 +0200

    added ability to view order in cart

--- foorpp/models.py
+++ foorpp/models.py
@@ -1,3 +1,4 @@
+from flask import session
 from foorpp import db
 from functools import total_ordering
 from sqlalchemy import func
@@ -15,6 +16,13 @@ menu_item_allergens = db.Table("menu_item_allergens",
                                          db.ForeignKey("menu_item.id")),
                                db.Column("allergen_id", db.Integer,
                                          db.ForeignKey("allergen.id")))
+
+
+order_menu_items = db.Table("order_menu_items",
+                            db.Column("order_id", db.Integer,
+                                      db.ForeignKey("order.id")),
+                            db.Column("menu_item_id", db.Integer,
+                                      db.ForeignKey("menu_item.id")))
 
 
 class Category(db.Model):
@@ -67,6 +75,52 @@ class CustomerSession(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     start = db.Column(db.DateTime, nullable = False, default = func.now())
     end = db.Column(db.DateTime)
+    order = db.relationship("Order", backref = "customer_session",
+                            uselist = False)
 
     def __repr__(self):
         return f"CustomerSession({self.id}, '{self.start}', '{self.end}')"
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    number = db.Column(db.Integer, nullable = False, default = 0)
+    time = db.Column(db.DateTime)
+    total_price = db.Column(db.Double, nullable = False, default = 0)
+    item_count = db.Column(db.Integer, nullable = False, default = 0)
+    status = db.Column(db.String(20), nullable = False,
+                        default = "unsubmitted")
+    session_id = db.Column(db.Integer, db.ForeignKey("customer_session.id"))
+    items = db.relationship("MenuItem", secondary = "order_menu_items",
+                            backref = "order")
+
+    def __repr__(self):
+        return f"Order({self.id}, {self.number}, {self.total_price},"\
+               f"{self.items}, {self.item_count}, '{self.status}')"
+
+    @classmethod
+    def get_current_order(self):
+        if session.get("order_id") is None:
+            current_order = Order()
+            db.session.add(current_order)
+            db.session.commit()
+            session["order_id"] = current_order.id
+            return current_order
+
+        return Order.query.get(session["order_id"])
+
+    def add_item(self, item):
+        self.total_price += item.price
+        self.item_count += 1
+        self.items.append(item)
+
+    def remove_item(self, item):
+        self.total_price -= item.price
+        self.item_count -= 1
+        self.item.remove(item)
+
+    def empty_cart(self):
+        self.total_price = 0
+        self.item_count = 0
+        self.items.clear()
+        db.session.commit()
--- foorpp/routes.py
+++ foorpp/routes.py
@@ -9,17 +9,18 @@ from flask import (
     url_for
 )
 from foorpp import app, db
-from foorpp.forms import SearchForm
 from foorpp.filters import filter_by_keyword
-from foorpp.models import Category, MenuItem, CustomerSession
+from foorpp.models import Category, CustomerSession, MenuItem, Order
 from sqlalchemy import func
 
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
+    session.clear()
     if "id" in session:
-        CustomerSession.query.filter(CustomerSession.id == session["id"]).first().end = func.now()
-        session.pop("id", None)
+        if session["id"] is not None:
+            CustomerSession.query.get(session["id"]).end = func.now()
+        session.clear()
 
     if request.method == "POST":
         if request.form.get("admin_login_btn"):
@@ -39,7 +40,7 @@ def index():
 
 @app.route("/categories", methods = ["GET", "POST"])
 def categories():
-    if "id" not in session:
+    if session.get("id") is None:
         return redirect(url_for("index"))
 
     if request.method == "POST":
@@ -62,7 +63,7 @@ def admin_login():
 
 @app.route("/menu", methods = ["GET", "POST"])
 def menu():
-    if "id" not in session:
+    if session.get("id") is None:
         return redirect(url_for("index"))
 
     filter_arg = ""
@@ -70,13 +71,12 @@ def menu():
     if data:
         filter_arg = data[0]
 
-    search = SearchForm()
     menu_items = filter_by_keyword(filter_arg.strip().lower())
 
     return render_template("menu.html", items = menu_items)
 
 
-@app.route("/search", methods = ["POST"])
+@app.post("/search")
 def search():
     flash(request.form["searched"])
     return redirect("menu")
@@ -84,22 +84,42 @@ def search():
 
 @app.route("/item/<item_id>")
 def item(item_id):
-    if "id" not in session:
+    if session.get("id") is None:
         return redirect(url_for("index"))
 
-    menu_item = MenuItem.query.filter(MenuItem.id == item_id).first()
+    menu_item = MenuItem.query.get(item_id)
     if menu_item is None:
         abort(404)
 
     return render_template("item.html", item = menu_item)
 
 
-@app.route("/cart")
+@app.route("/cart", methods = ["GET", "POST"])
 def cart():
-    if "id" not in session:
+    if session.get("id") is None:
         return redirect(url_for("index"))
 
-    return render_template("cart.html")
+    order = Order.get_current_order()
+
+    return render_template("cart.html", order=order)
+
+
+@app.post("/add_to_cart/<origin>/<item_id>")
+def add_to_cart(origin, item_id):
+    order = Order.get_current_order()
+    order.add_item(MenuItem.query.get(item_id))
+    db.session.commit()
+
+    if (origin == "item"):
+        return redirect(url_for("item", item_id = item_id))
+
+    return redirect(url_for("menu"))
+
+
+@app.post("/empty_cart")
+def empty_cart():
+    Order.query.get(session["order_id"]).empty_cart()
+    return redirect(url_for("cart"))
 
 
 @app.errorhandler(404)
